382871f462b9bf7af59389247b87ba4d25e3b581
arza.3d <arza.3d@gmail.com>
2018-11-14 17:01:36 +0700

    the first basic game work (without input)

--- blackjack/blackjack.js
+++ blackjack/blackjack.js
@@ -10,32 +10,56 @@
     function create1Deck() {
       for (let i = 0; i < cardTypes.length; i++) {
         for (let j = 1; j <= 13; j++) {
-          mainDecks.push(cardTypes[i] + '_' + j);
+          mainDecks.push(j + ' ' + cardTypes[i]);
         }
       }
     }
 
-    // random number function based on mainDecks
+    // pull card from main deck
     function pullCard() {
       let cardPull = Math.round(Math.random()*mainDecks.length);
       return mainDecks.splice(cardPull, 1);
     }
 
-    // give card to player and ai
     function giveCardTo(deck = playerDeck) {
       for (let i = 0; i < 2; i++) {
         deck.push(pullCard());
       }
     }
 
-    // check if still can play (minimum)
     function isStillCanPlay(cardMin = 7) {
       return mainDecks.length() > cardMin;
     }
 
-    // count card
-    function countCard() {
-      
+    function countCard(deck = playerDeck) {
+      let cardCount = 0;
+      for (let i = 0; i < deck.length; i++) {
+        cardCount += parseInt(deck[i]);
+      }
+      return cardCount;
+    }
+
+    function checkWinStatus() {
+      let playerCount, aiCount;
+      playerCount = countCard();
+      aiCount = countCard(aiDeck);
+      if (playerCount > 21) {
+        if (aiCount > 21) {
+          return 'draw';
+        } else {
+          return 'lose';
+        }
+      } else {
+        if (aiCount > 21) {
+          return 'win';
+        } else if (playerCount > aiCount) {
+          return 'win';
+        } else if (playerCount < aiCount) {
+          return 'lose';
+        } else {
+          return 'draw';
+        }
+      }
     }
 
   }
@@ -48,6 +72,9 @@
     console.log(playerDeck);
     console.log(aiDeck);
     console.log(mainDecks);
+    console.log('player card count is ' + countCard());
+    console.log('ai card count is ' + countCard(aiDeck));
+    console.log(checkWinStatus());
   }
 
 }
