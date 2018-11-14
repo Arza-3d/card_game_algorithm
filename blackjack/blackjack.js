97a033f43c6a6396334adaa6d6724464a65973f1
arza.3d <arza.3d@gmail.com>
2018-11-14 20:03:34 +0700

    card count with ace

--- blackjack/blackjack.js
+++ blackjack/blackjack.js
@@ -2,15 +2,23 @@
   let mainDecks = [],
       playerDeck = [],
       aiDeck = [];
-  const cardTypes = ['club', 'heart', 'diamond', 'spade'];
+
 
   // function definitions:
   {
     // create card deck
+    const cardTypes = ['club', 'heart', 'diamond', 'spade'];
+    const cardUnique = ['ace', 'jack', 'queen', 'king'];
     function create1Deck() {
       for (let i = 0; i < cardTypes.length; i++) {
-        for (let j = 1; j <= 13; j++) {
+        mainDecks.push(cardUnique[0] + ' ' + cardTypes[i]);
+
+        for (let j = 2; j <= 10; j++) {
           mainDecks.push(j + ' ' + cardTypes[i]);
+        }
+
+        for (let j = 1; j < cardUnique.length; j++) {
+          mainDecks.push(10 + ' ' + cardUnique[j] + ' ' + cardTypes[i]);
         }
       }
     }
@@ -27,10 +35,6 @@
       }
     }
 
-    function isStillCanPlay(cardMin = 7) {
-      return mainDecks.length > cardMin;
-    }
-
     function countCard(deck = playerDeck) {
       let cardCount = 0;
       for (let i = 0; i < deck.length; i++) {
@@ -39,10 +43,52 @@
       return cardCount;
     }
 
+    function hasAce(deck = playerDeck) {
+      if (isNaN(countCard(deck))) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    function howManyAce(deck = playerDeck) {
+      let aceCount = 0;
+      for (let i = 0; i < deck.length; i++) {
+        if (isNaN(parseInt(deck[i]))) {
+          aceCount++;
+        }
+      }
+      return aceCount;
+    }
+
+    function countCardWithAce(deck = playerDeck) {
+      let tempDeck = deck.concat([]);
+      if (hasAce(tempDeck)) {
+        let aceCount = howManyAce(tempDeck);
+        let onlyAcetempDeck = [];
+        for (let i = 0; i < tempDeck.length; i++) {
+          if (isNaN(parseInt(tempDeck[i]))) {
+            onlyAcetempDeck.push(tempDeck.splice(i, 1));
+          }
+        }
+        let countWithoutAce = countCard(tempDeck);
+        let countOnlyAce = 11 + (aceCount - 1);
+        if (countWithoutAce == 0) {
+          return countOnlyAce;
+        } else if (countWithoutAce <= (21 - countOnlyAce)) {
+          return countWithoutAce + countOnlyAce;
+        } else {
+          return countWithoutAce + aceCount;
+        }
+      } else {
+        return countCard(tempDeck);
+      }
+    }
+
     function checkWinStatus() {
       let playerCount, aiCount;
-      playerCount = countCard();
-      aiCount = countCard(aiDeck);
+      playerCount = countCardWithAce();
+      aiCount = countCardWithAce(aiDeck);
       if (playerCount > 21) {
         if (aiCount > 21) {
           return 'draw';
@@ -67,6 +113,10 @@
       aiDeck = [];
     }
 
+    function isStillCanPlay(cardMin = 7) {
+      return mainDecks.length > cardMin;
+    }
+
   }
 
   // play
@@ -78,11 +128,11 @@
         clearPlayersDeck();
         giveCardTo();
         giveCardTo(aiDeck);
-        //console.log(playerDeck);
-        //console.log(aiDeck);
-        //console.log(mainDecks);
-        console.log('player card count is ' + countCard());
-        console.log('ai card count is ' + countCard(aiDeck));
+        console.log(playerDeck);
+        console.log(aiDeck);
+        console.log(mainDecks);
+        console.log('player card count is ' + countCardWithAce());
+        console.log('ai card count is ' + countCardWithAce(aiDeck));
         console.log(checkWinStatus());
       } else {
         console.log('can\'t play anymore');
@@ -91,5 +141,4 @@
     playBlackJack();
 
   }
-
 }
